d8106575cdc76fc1c0f759b03d39bb70b73d6f9c
Lenovo <Lenovo@192.168.10.3>
2018-10-02 23:00:07 +0200

    Изи рил ток синк эбаут ит

---
+++ Main/src/MyClass.java
@@ -0,0 +1,18 @@
+
+public class MyClass {   //строчный коммент. Главный класс в котором выполняется программа.
+
+	public static void main(String[] args) {    //основной метод при помощи которого выполняется программа.
+		for(int i=1;i<=100;i++)                 //цикл. перебираем переменную
+				System.out.print(i + " ");      // вывод на консоль
+	}
+	
+	/*блочный коммент
+	public class MyClass {
+	*/
+	
+	/* 
+	 * public static void main(String[] args) {    
+		for(int i=1;i<=100;i++)                   
+				System.out.print(i + " ");        
+	}*/
+}

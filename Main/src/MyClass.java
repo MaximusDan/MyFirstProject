f43f11d36f0326985bd8620450204b603ed4c784
Lenovo <Lenovo@192.168.10.4>
2018-10-03 21:04:29 +0200

    #1 Удалить лишний класс

--- Main/src/MyClass.java
+++ Main/src/MyClass.java
@@ -1,18 +1,31 @@
+import java.util.Scanner;
 
-public class MyClass {   //строчный коммент. Главный класс в котором выполняется программа.
+public class MyClass {   
 
-	public static void main(String[] args) {    //основной метод при помощи которого выполняется программа.
-		for(int i=1;i<=100;i++)                 //цикл. перебираем переменную
-				System.out.print(i + " ");      // вывод на консоль
-	}
+	//Задача 1
+	public static void main(String[] args) {    
+		for(int i=1;i<=100;i++)                 
+				System.out.print(i + " ");      
 	
-	/*блочный коммент
-	public class MyClass {
-	*/
-	
-	/* 
-	 * public static void main(String[] args) {    
-		for(int i=1;i<=100;i++)                   
-				System.out.print(i + " ");        
-	}*/
+	//Задача 2
+		
+	int x = 1, x1 = 1;                                   
+	System.out.println();
+	while(true) {
+		Scanner sc = new Scanner(System.in);    
+		Scanner sc1 = new Scanner(System.in);   
+		System.out.println("Введите правильно 1 число");
+		System.out.println("Введите правильно 2 число");
+		if (sc.hasNextInt() && sc1.hasNextInt()) {            
+			x = sc.nextInt();                          
+			x1 = sc1.nextInt();                         
+			
+			System.out.println("сложение = " + (x + x1));
+			System.out.println("умножение = " + (x * x1));
+			System.out.println("деление = " + (x / x1));
+			System.out.println("вычитание = " + (x - x1));
+			break;
+}
+}
+}
 }

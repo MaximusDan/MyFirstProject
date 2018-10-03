cab9423408e345748a7fd5b8c69dc74ea4d9c328
Lenovo <Lenovo@192.168.10.4>
2018-10-04 00:08:12 +0200

    #6 Делители числа #5 Проверки (Исправление)

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -14,18 +14,15 @@ public class Main {
 	 * Главный метод
 	 * Цикл for перебирающий значения от 1-100 */
 	public static void main(String[] args) {
-		for (int i = 1; i <= 100; i++) {
-			System.out.print(i + " ");
-		}
+		//for (int i = 1; i <= 100; i++) {
+			//System.out.print(i + " ");
+		//}
 		
 		/** 
 		 * ЗАДАЧА 2
 		 * Метод While
 		 * Создание 2-х объектов класса Scanner
-		 * Внесение объектов в переменные
-		 * арифметические действия с переменными
 		 *  */
-		// Задача 2
 		int value = 0, value1 = 0;
 		System.out.println();
 		while (true) {
@@ -34,19 +31,49 @@ public class Main {
 			System.out.println("Введите правильно 1 число");
 			System.out.println("Введите правильно 2 число");
 			
-			if (sc.hasNextInt() && sc1.hasNextInt() && value1 > 0) {
+			 /**Проверка, что обьекты имеют тип int
+			 * Внесение объекта в переменную*/
+			if (sc.hasNextInt() && sc1.hasNextInt()) {
 				value = sc.nextInt();
 				value1 = sc1.nextInt();
-
-				System.out.println("сложение = " + (value + value1));
-				System.out.println("умножение = " + (value * value1));
-				System.out.println("деление = " + (value / value1));
-				System.out.println("вычитание = " + (value - value1));
-				break;
-			}else {
-				System.out.println("Одно из введенных чисел неверно" );
+				
+				 /**Проверка, что делитель не является нулем
+				 * Арифметические действия*/
+				if (value1 != 0) {
+					System.out.println("сложение = " + (value + value1));
+					System.out.println("умножение = " + (value * value1));
+					System.out.println("деление = " + (value / value1));
+					System.out.println("вычитание = " + (value - value1));
+					break;
+				} else {
+					System.out.println("Одно из введенных чисел неверно");
+				}
 			}
-			
+		} 
+		
+		/**Делители числа #6
+		 * Ввести с консоли новое числа и вывести все делители этого числа( делители числа - это те числа, на которые оно делится без остатка)*/
+		int value2 = 0;
+		System.out.println();
+		Scanner sc2 = new Scanner(System.in);
+		System.out.println("Введите число");
+		while(true) {
+			/**Проверка, что обьект имеет тип int*/
+			if(sc2.hasNextInt()) {
+				value2 = sc2.nextInt();
+				
+				/**Проверяем что бы переменная не была нолем и выводим делители*/
+			if(value2!=0) {
+				for (int i = 1; i <= value2; i++) {
+					if ((value2 % i) == 0) {
+						System.out.println(i);
+					}
+				}
+			}else {
+				System.out.println("Введите другое число");
+			}
+			}
 		}
 	}
+
 }
